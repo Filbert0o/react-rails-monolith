051cad0ea3a386a638fed82908b36321f12b8945
Timothy Andrew Filbert <timfilbert97@gmail.com>
2017-12-03 19:23:36 -0500

    Initial Commit

---
+++ react/src/main.js
@@ -0,0 +1,17 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+$(function() {
+  ReactDOM.render(
+    <h1>WHATTHEF</h1>,
+    document.getElementById('app')
+  );
+});
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(
+//     <h1>Boo yaa</h1>,
+//     document.getElementById('app'));
+// })
